5853b4d542239fe079ab7d3b4939b48b7231103e
Ines <inesucrvenom@gmail.com>
2020-07-31 20:49:26 +0200

    add year and task description

--- short-exercises/python/open_labyrinth.py
+++ short-exercises/python/open_labyrinth.py
@@ -1,6 +1,6 @@
 import random
 
-"""  https://docs.google.com/document/d/1D51moWM_TyfWQepWCLn3_AMngxZd_u9AFIAognrDIfg/edit?usp=sharing """
+""" Made in 2015 """
 
 """Using generative recursion search"""
 # all possible moves
@@ -11,6 +11,7 @@ START = [1, 1]
 END = [10, 10]
 PIT = 1
 
+
 def find(path, board):
     """Generating arbitrary arity tree + backtracking search over it"""
     def find__path(path):
@@ -20,6 +21,7 @@ def find(path, board):
             return path
         else:
             return find__lopath(next_paths(path, board))
+
     def find__lopath(lopath):
         """Test each path in given list if is it solution"""
         if len(lopath) == 0:
@@ -33,6 +35,7 @@ def find(path, board):
                 return find__lopath(lopath[1:])
     return find__path(path)
 
+
 def is_found(path):
     """Return True if solution is found - end position is reached"""
     if path == "":
@@ -40,50 +43,54 @@ def is_found(path):
     else:
         return path_to_pos(path) == END
 
+
 def path_to_pos(path):
-    """Return position that path reached""" 
+    """Return position that path reached"""
     pos = list(START)
     for d in path:
         pos[0] += moves[d][0]
         pos[1] += moves[d][1]
     return pos
 
+
 def value_from_pos(pos, board):
     return board[pos[0]][pos[1]]
+
 
 def next_paths(path, board):
     """Return list of valid next paths from given
     Assume: only move forward, no returning back in same path"""
     # generate path candidates
     candidates = [path + m for m in moves]
-    
+
     # filter paths that don't fall into PIT
     # and are not going in opposite direction
     valid_paths = []
     for c in candidates:
         if (len(c)) >= 2:
-            if ((value_from_pos(path_to_pos(c), board) !=  PIT)
-                and c[-2] != opposite[c[-1]]):
+            if ((value_from_pos(path_to_pos(c), board) != PIT)
+                    and c[-2] != opposite[c[-1]]):
                 valid_paths.append(c)
-        elif (value_from_pos(path_to_pos(c), board) !=  PIT):
+        elif (value_from_pos(path_to_pos(c), board) != PIT):
             valid_paths.append(c)
-    
+
     # filter out paths that have cycle
     valid_paths = [c for c in list(valid_paths)
                    if not has_cycle(c, board)]
 
     return valid_paths
 
+
 def has_cycle(path, board):
     """Return True if path has cycle - has duplicate positions"""
     sub_paths = [path[:i+1] for i in range(len(path))]
     list_pos = [path_to_pos(p) for p in sub_paths]
-    non_unique  = [d for d in list_pos if list_pos.count(d) > 1]
+    non_unique = [d for d in list_pos if list_pos.count(d) > 1]
     if len(non_unique):
         return True
     else:
         return False
 
+
 def checkio(maze_map):
     return find("", maze_map)
-
